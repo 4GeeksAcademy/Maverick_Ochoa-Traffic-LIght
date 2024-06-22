c2aee8f9361a15849382e669f2e20788034550e6
MaverickOchoa <126739291+MaverickOchoa@users.noreply.github.com>
2024-06-22 14:27:43 +0000

    finished Traffic Light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,54 @@
 
-import React, { useState} from "react";
+import React, { useEffect, useState} from "react";
 
 
 
 //create your first component
 const Home = () => {
 	const [activeLight, setActiveLight] = useState(null);
-	return (
-		<div className="traffic-light mt-5">
-			<div
-				className={`light red ${activeLight === 'red' ? 'active' : ''}`}
-				onClick={() => setActiveLight('red')}
-			/>
-			<div
-				className={`light yellow ${activeLight === 'yellow' ? 'active' : ''}`}
-				onClick={() => setActiveLight('yellow')}
-			/>
-			<div
-				className={`light green ${activeLight === 'green' ? 'active' : ''}`}
-				onClick={() => setActiveLight('green')}
-			/>
-			
+	const [isRinning, setIsRunning] = useState(false);
 
-    	</div>
+	const lights = ['red', 'yellow', 'green'];
+
+	useEffect(() =>{
+		let interval;
+		if(isRinning){
+			interval = setInterval(()=> {
+				const randomLight = lights[Math.floor(Math.random() * lights.length)];
+				setActiveLight(randomLight);
+			}, 500);
+		}
+		return () => clearInterval(interval)
+	},[isRinning]);
+
+	const toggleTrafficLight =()=>{
+		setIsRunning(!isRinning)
+	}
+
+	return (
+		<div className="col-12 mb-5">
+			<div className="pole col-1">
+			</div>
+			<div className="traffic-light">
+				<div
+					className={`light red ${activeLight === 'red' ? 'active' : ''}`}
+					onClick={() => setActiveLight('red')}
+				/>
+				<div
+					className={`light yellow ${activeLight === 'yellow' ? 'active' : ''}`}
+					onClick={() => setActiveLight('yellow')}
+				/>
+				<div
+					className={`light green ${activeLight === 'green' ? 'active' : ''}`}
+					onClick={() => setActiveLight('green')}
+				/>
+    		</div>
+
+			<div className="col-12 mt-5 d-flex justify-content-center " >
+				<button className="btn btn-danger px-5" onClick={toggleTrafficLight}>{isRinning ? 'Stop' : 'Start'}</button>
+			</div>
+		</div>
+		
 
 	);
 };
